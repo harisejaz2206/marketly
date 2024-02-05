6da786aae92e3e954e60e8d35c3df28eb31c42fd
harisejaz2206 <harisejaz2206@gmail.com>
2024-02-05 23:29:01 +0500

    Made MaxWidthWrapper, starting work next day

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <p>Hello world</p>
+    <MaxWidthWrapper>
+      <div className=" py-20 mx-auto text-center flex flex-col items-center max-3xl">
+        <h1>
+          Your marketplace for high quality {''}
+          <span className="text-blue-600">Digital Assets</span>
+        </h1></div>
+    </MaxWidthWrapper>
   );
 }
