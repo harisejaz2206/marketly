0df4d47d763ddbdecff3d3d9838eaaee7830a77a
harisejaz2206 <harisejaz2206@gmail.com>
2024-02-09 20:55:37 +0500

    Cart component done | started working on CMS (backend)

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -11,3 +11,31 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+/**
+ * Formats the given price into a currency format according to specified options.
+ * @param {number | string} price The price to format. Can be a number or a string representing a number.
+ * @param {Object} [options] The formatting options.
+ * @param {"USD" | "EUR" | "GBP" | "BDT"} [options.currency="USD"] The currency to use for formatting. Default is USD.
+ * @param {Intl.NumberFormatOptions['notation']} [options.notation="compact"] The notation style to use. Default is "compact".
+ * @returns {string} The formatted price as a string in the specified currency format.
+ */
+export function formatPrice(
+  price: number | string, 
+  options: {
+    currency?: "USD" | "EUR" | "GBP" | "BDT",
+    notation?: Intl.NumberFormatOptions['notation']
+    } = {}
+  ) {
+    const {currency = 'USD', notation = 'compact'} = options;
+
+   const numericPrice = typeof price === 'string' ? parseFloat(price) : price;
+
+   return Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: currency,
+    notation,
+    maximumFractionDigits:2
+   }).format(numericPrice)
+  }
