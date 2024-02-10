e2c2d1e07e91813d96986743bb30f05342f5abd4
harisejaz2206 <harisejaz2206@gmail.com>
2024-02-10 23:54:04 +0500

    User collection made | added role

--- src/trpc/auth.router.ts
+++ src/trpc/auth.router.ts
@@ -1,12 +1,33 @@
-import { AuthCredentialsValidator } from "@/lib/form-validators/account-credentials.validator";
+import { AuthCredentialsValidator } from "../lib/form-validators/account-credentials.validator";
 import { publicProcedure, router } from "./trpc";
-import { getPayloadClient } from "@/get.payload";
+import { getPayloadClient } from "../get.payload";
+import { TRPCError } from "@trpc/server";
 
 export const authRouter = router ({
     createPayloadUser: publicProcedure
     .input(AuthCredentialsValidator)
     .mutation( async ({input})=>{
         const {email, password} = input;
-        const payload = await getPayloadClient   
+        const payload = await getPayloadClient()
+
+        // check if the user already exists
+        const {docs: users} = await payload.find({
+            collection: "users",
+            where: {
+                email: {
+                    equals: email,
+                }
+            }
+        })
+
+        if(users.length !==0) throw new TRPCError({code: "CONFLICT"})
+
+        await payload.create({
+            collection: "users",
+            data: {
+                
+            }
+        })
+
     })
 })
--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,3 +1,4 @@
+import { authRouter } from "./auth.router";
 import { publicProcedure, router } from "./trpc";
 
 /**
@@ -9,9 +10,7 @@ import { publicProcedure, router } from "./trpc";
  */
 
 export const appRouter = router({
-    anyApiRoute: publicProcedure.query(()=>{
-        return "hello"
-    })
+    auth: authRouter
 })
 
 export type AppRouter = typeof appRouter;
